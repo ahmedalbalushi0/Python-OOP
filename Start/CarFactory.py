7d9ce87301ac3420243fd66935beaf78122e7a26
Ahmed <ahmed1balushi3@gmail.com>
2026-07-27 22:37:55 +0400

    car oop practice in a separate branch

---
+++ Start/CarFactory.py
@@ -0,0 +1,8 @@
+class Car:
+    color = "blue"
+    brand = "Ford"
+    model = "2018"
+
+#object/instance
+Car1 = Car()
+print("Color : ",Car1.color, "\nBrand : ", Car1.brand,"\nModel : ", Car1.model)
