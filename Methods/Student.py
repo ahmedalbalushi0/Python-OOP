336d0ff37379f7d54577bff15cf3927b878bcf54
Ahmed <ahmed1balushi3@gmail.com>
2026-08-06 00:39:21 +0400

    using static method

--- Methods/Student.py
+++ Methods/Student.py
@@ -5,6 +5,10 @@ class Student:
         self.marks = marks
         print("Welcome",self.name)
 
+    @staticmethod
+    def uni_name():
+        print("UTAS Muscat")
+
     def Average(self):
         sum=0
         for val in self.marks:
@@ -12,5 +16,6 @@ class Student:
         print("Hi",self.name,"your average score is",sum/3)    
         
 s1 = Student("Mohammed", [89,91,78])
+s1.uni_name()
 print("\nName:",s1.name,"\nMarks:",s1.marks)
 s1.Average()
