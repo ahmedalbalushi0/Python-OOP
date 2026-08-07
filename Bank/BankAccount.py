0a77dea4f8ddf19a7a40728c8ed896a98faddb31
Ahmed <ahmed1balushi3@gmail.com>
2026-08-07 21:53:42 +0400

    added reset_pass method to view private password

--- Bank/BankAccount.py
+++ Bank/BankAccount.py
@@ -5,6 +5,11 @@ class Account:
         self.__acc_pass = acc_pass #private attribute
         self.balance = bal
 
+    #reset password method
+    def reset_pass(self, new_pass):
+        self.__acc_pass = new_pass
+        print("Password updated successfully")
+
     #debit method
     def debit(self,amount):
         self.balance -=amount
@@ -14,7 +19,7 @@ class Account:
     #credit method
     def credit(self,amount):
         self.balance += amount
-        print(amount,"OMR was credited to your accont",self.account_no)
+        print(amount,"OMR was credited to your account",self.account_no)
         print("Current balance:",self.get_balance())
 
     #final balance
@@ -25,3 +30,4 @@ acc1 = Account(13864761,"F5pi1",0)
 print("Account No. :",acc1.account_no,"\nAcount Balance:",acc1.balance)
 acc1.credit(750)
 acc1.debit(110)
+acc1.reset_pass("r501Qf")
